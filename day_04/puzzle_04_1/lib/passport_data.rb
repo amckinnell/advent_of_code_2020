1e51a7791b4d63041b23b5895ef02ff38c2470a3
Alistair McKinnell <alistairm@nulogy.com>
2020-12-04 16:09:59 -0500

    Stub in the PassportData

--- day_04/puzzle_04_1/lib/passport_data.rb
+++ day_04/puzzle_04_1/lib/passport_data.rb
@@ -1,3 +1,31 @@
+#
+# Knows how to parse the Password Data input file
+#
+
 class PassportData
-  # TBD
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def transform_normalized_passport_data
+    normalized_passport_data.map do |passport_data|
+      passport_data.split.map { |d| d.split(":") }.to_h.transform_keys(&:to_sym)
+    end
+  end
+
+  private
+
+  def normalized_passport_data
+    raw_passport_data.each_with_object([""]) do |line, memo|
+      if line.empty?
+        memo << ""
+      else
+        memo.last << " #{line}"
+      end
+    end
+  end
+
+  def raw_passport_data
+    @_raw_passport_data ||= File.foreach(@filename).map { |line| line.chomp }
+  end
 end
--- day_04/puzzle_04_1/spec/passport_data_spec.rb
+++ day_04/puzzle_04_1/spec/passport_data_spec.rb
@@ -1,5 +1,7 @@
 RSpec.describe PassportData do
   it "works" do
-    raise "TBD"
+    passport_data = PassportData.new("../input_04.txt")
+
+    transform_normalized_passport_data = passport_data.transform_normalized_passport_data
   end
 end
