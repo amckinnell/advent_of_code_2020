53370bf877a3f0119f1c81805866772841d7374a
Alistair McKinnell <alistairm@nulogy.com>
2020-12-05 15:48:07 -0500

    Implement finding the missing seat
    - Counts on the missing seat ID being neither first or last

--- day_05/puzzle_05_2/main.rb
+++ day_05/puzzle_05_2/main.rb
@@ -8,5 +8,10 @@ missing_boarding_pass_codes = boarding_pass.all - boarding_pass_codes
 missing_seat_ids = missing_boarding_pass_codes
   .map { |missing_boarding_pass_code| boarding_pass.seat_id(missing_boarding_pass_code) }
 
-p "#################### missing_seat_ids ####################"
-p missing_seat_ids
+pruned_from_front_missing_seat_ids =
+  missing_seat_ids.drop_while.with_index { |id, i| id + 1 == missing_seat_ids[i + 1] }
+
+pruned_from_back_missing_seat_ids =
+  missing_seat_ids.reverse.drop_while.with_index { |id, i| id - 1 == missing_seat_ids.reverse[i + 1] }
+
+p (pruned_from_front_missing_seat_ids & pruned_from_back_missing_seat_ids)[1]
