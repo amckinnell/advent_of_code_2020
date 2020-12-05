62d3d6d2a63a51aa3aa6fe16794d186b38f95a97
Alistair McKinnell <alistairm@nulogy.com>
2020-12-05 11:26:14 -0500

    Copies from puzzle 1

---
+++ day_04/puzzle_04_2/lib/passport_data_classifier.rb
@@ -0,0 +1,22 @@
+#
+# Knows if the specified Password Data is valid.
+#
+class PassportDataClassifier
+  REQUIRED_FIELDS = [
+    :byr,
+    :ecl,
+    :eyr,
+    :hcl,
+    :hgt,
+    :iyr,
+    :pid
+  ]
+
+  OPTIONAL_FIELDS = [
+    :cid
+  ]
+
+  def valid?(passport_data)
+    (REQUIRED_FIELDS - passport_data.keys).empty?
+  end
+end
