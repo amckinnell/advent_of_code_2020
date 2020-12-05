e43c56747d1012272a096337d070132149a0b278
Alistair McKinnell <alistairm@nulogy.com>
2020-12-05 11:10:46 -0500

    Move main to the puzzle directory

--- day_04/puzzle_04_1/main.rb
+++ day_04/puzzle_04_1/main.rb
@@ -1,4 +1,4 @@
-require_relative "../configure_zeitwerk"
+require_relative "configure_zeitwerk"
 
 passport_data = PassportData.new("../input_04.txt")
 passport_data_classifier = PassportDataClassifier.new
