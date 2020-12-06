608a6793b55fdbdf74e14b68fc541265c54a76ee
Alistair McKinnell <alistairm@nulogy.com>
2020-12-06 08:50:20 -0500

    Small improvements
    - Use detect rather than select + first
    - Both inject and reduce accept just a symbol (no need for &)
    - Use readlines(name, chomp: true)
    - Use lambda rather method

--- day_02/puzzle_02_2.rb
+++ day_02/puzzle_02_2.rb
@@ -1,10 +1,10 @@
-def valid_password?(line)
+valid_password = ->(line) {
   m = /(?<first>\d+)-(?<second>\d+) (?<character>[a-z]): (?<password>.+)/.match(line)
 
   index_1 = m[:first].to_i - 1
   index_2 = m[:second].to_i - 1
 
   (m[:password][index_1] == m[:character]) ^ (m[:password][index_2] == m[:character])
-end
+}
 
-puts File.foreach("input_02.txt").count { |line| valid_password?(line.chomp) }
+p File.readlines("input_02.txt", chomp: true).count(&valid_password)
