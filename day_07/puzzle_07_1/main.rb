cd9a900f8b200f90d8963fe5648beba0faa33075
Alistair McKinnell <alistairm@nulogy.com>
2020-12-07 15:40:50 -0500

    Implement BagRegulationsInput

--- day_07/puzzle_07_1/main.rb
+++ day_07/puzzle_07_1/main.rb
@@ -1,3 +1,9 @@
 require_relative "configure_zeitwerk"
 
-# TBD: Top-level script goes here
+bag_regulation_input = BagRegulationsInput.new(File.read("../input_07.txt")).parsed_input
+
+bag_regulations = BagRegulations.new(bag_regulation_input)
+
+outermost_bag_colors = bag_regulations.find_outermost_bag_colors
+
+p outermost_bag_colors.count
