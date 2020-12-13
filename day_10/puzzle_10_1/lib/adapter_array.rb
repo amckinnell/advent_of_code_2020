fbe9fb124f1f708ffa044f4679e81a57211d6e38
Alistair McKinnell <alistairm@nulogy.com>
2020-12-13 10:22:28 -0500

    Implement puzzle 1 Day 10

--- day_10/puzzle_10_1/lib/adapter_array.rb
+++ day_10/puzzle_10_1/lib/adapter_array.rb
@@ -6,4 +6,34 @@ class AdapterArray
   def joltage_rating
     -1
   end
+
+  def longest_adapter_sequence
+    begin
+      search(current_output: 0, adaptor_sequence: [], unused_adapters: @data)
+    rescue FoundAdaptorSequence => e
+      return e.adapter_sequence
+    end
+
+    raise "No complete adapter sequence found"
+  end
+
+  def search(current_output:, adaptor_sequence:, unused_adapters:)
+    raise FoundAdaptorSequence.new(adaptor_sequence) if unused_adapters.empty?
+
+    next_adapters = eligible_adapters(current_output, unused_adapters)
+
+    return adaptor_sequence if next_adapters.empty?
+
+    next_adapters.each do |next_adapter|
+      search(
+        current_output: next_adapter,
+        adaptor_sequence: adaptor_sequence.append(next_adapter),
+        unused_adapters: unused_adapters - [next_adapter]
+        )
+    end
+  end
+
+  def eligible_adapters(current_output, adapters)
+    adapters.select { |adapter| ((current_output) + 1..(current_output + 3)).cover?(adapter) }.sort
+  end
 end
--- day_10/puzzle_10_1/main.rb
+++ day_10/puzzle_10_1/main.rb
@@ -1,3 +1,10 @@
-require_relative "configure_zeitwerk"
+adapter_data = File.read("../input_10.txt").lines.map(&:to_i).sort
 
-# TBD: Top-level script goes here
+previous_adapters = adapter_data.dup.prepend(0)
+adapters = adapter_data.dup.append(adapter_data.max + 3)
+
+joltage_differences = adapters.zip(previous_adapters).map { |adapter, previous_adapter| adapter - previous_adapter }
+
+solution = joltage_differences.tally.slice(1, 3).values.reduce(:*)
+
+p solution
