1d304f9127eb0c84f88f0584bb04a917d9637116
Alistair McKinnell <alistairm@nulogy.com>
2021-01-17 23:27:46 -0500

    First simple case

--- day_18/puzzle_18_1/lib/operation_order.rb
+++ day_18/puzzle_18_1/lib/operation_order.rb
@@ -1,3 +1,20 @@
 class OperationOrder
-  # TBD
+  def initialize(raw_data)
+    @raw_data = raw_data
+
+  end
+
+  def evaluate
+    @raw_data.lines.map { |raw_line| evaluate_line(tokenize(raw_line.chomp)) }
+  end
+
+  private
+
+  def evaluate_line(tokens)
+    tokens.first.to_i
+  end
+
+  def tokenize(line)
+    line.split(" ")
+  end
 end
--- day_18/puzzle_18_1/spec/operation_order_spec.rb
+++ day_18/puzzle_18_1/spec/operation_order_spec.rb
@@ -1,5 +1,24 @@
 RSpec.describe OperationOrder do
-  it "works" do
-    raise "TBD"
+  describe "simple cases without parentheses" do
+    it "minimal expression" do
+      operation_order = OperationOrder.new(<<~DATA)
+        2
+        3
+      DATA
+
+      result = operation_order.evaluate
+
+      expect(result).to eq([2, 3])
+    end
+
+    it "minimal expression with operator" do
+      operation_order = OperationOrder.new(<<~DATA)
+        1 + 2
+      DATA
+
+      result = operation_order.evaluate
+
+      expect(result).to eq([3])
+    end
   end
 end
