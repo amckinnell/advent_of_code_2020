66192785b66ee466751545a66e9cb8f0d0d897e8
Alistair McKinnell <alistairm@nulogy.com>
2021-01-20 06:27:37 -0500

    Solution without parentheses

--- day_18/puzzle_18_1/lib/operation_order.rb
+++ day_18/puzzle_18_1/lib/operation_order.rb
@@ -1,9 +1,22 @@
 class OperationOrder
-  def initialize(raw_line)
-    @raw_line = raw_line
+  attr_reader :expression
+
+  def initialize(expression)
+    @expression = expression
   end
 
   def evaluate
-    @raw_line.to_i
+    tokens = expression.split(" ")
+    operator_index = tokens.rindex { |token| ["+", "*"].include?(token) }
+
+    if operator_index
+      lhs = tokens.take(operator_index).join(" ")
+      operator = tokens[operator_index]
+      rhs = tokens.last.to_i
+
+      OperationOrder.new(lhs).evaluate.send(operator, rhs)
+    else
+      tokens.last.to_i
+    end
   end
 end
--- day_18/puzzle_18_1/spec/operation_order_spec.rb
+++ day_18/puzzle_18_1/spec/operation_order_spec.rb
@@ -1,34 +1,41 @@
 RSpec.describe OperationOrder do
   describe "simple cases" do
-    it "simple token" do
+    it "simple value" do
       operation_order = OperationOrder.new("2")
       result = operation_order.evaluate
 
       expect(result).to eq(2)
     end
 
-    xit "simple binary + expression" do
-      operation_order = OperationOrders.new("1 + 2")
+    it "simple binary + expression" do
+      operation_order = OperationOrder.new("1 + 2")
 
       result = operation_order.evaluate
 
       expect(result).to eq(3)
     end
 
-    xit "simple binary * expression" do
-      operation_order = OperationOrders.new("2 * 3")
+    it "simple binary * expression" do
+      operation_order = OperationOrder.new("2 * 3")
 
       result = operation_order.evaluate
 
       expect(result).to eq(6)
     end
 
-    xit "compound expression" do
-      operation_order = OperationOrders.new("2 + 2 * 3")
+    it "compound expression" do
+      operation_order = OperationOrder.new("2 + 2 * 3")
 
       result = operation_order.evaluate
 
       expect(result).to eq(12)
     end
+  end
+
+  it "example from problem statement" do
+    operation_order = OperationOrder.new("1 + 2 * 3 + 4 * 5 + 6")
+    result = operation_order.evaluate
+
+    expect(result).to eq(71)
   end
 end
